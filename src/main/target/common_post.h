2ba4d21584325eb56025882075efa71a018d4b7e
chemicstry <chemicstry@gmail.com>
2025-12-18 15:48:11 +0200

    Fix gyro defines

--- src/main/target/common_post.h
+++ src/main/target/common_post.h
@@ -428,7 +428,7 @@
 
 #if defined(USE_GYRO_SPI_ICM20689) || defined(USE_GYRO_SPI_MPU6000) || defined(USE_GYRO_SPI_MPU6500) || defined(USE_GYRO_SPI_MPU9250) \
     || defined(USE_GYRO_L3GD20) || defined(USE_GYRO_SPI_ICM42605) || defined(USE_GYRO_SPI_ICM42688P) || defined(USE_ACCGYRO_BMI160) \
-    || defined(USE_ACCGYRO_BMI270) || defined(USE_ACCGYRO_LSM6DSV16X) || defined(USE_ACCGYRO_LSM6DSO)
+    || defined(USE_ACCGYRO_BMI270) || defined(USE_ACCGYRO_LSM6DSV16X) || defined(USE_ACCGYRO_LSM6DSO) || defined(USE_ACCGYRO_QMI8658)
 #ifndef USE_SPI_GYRO
 #define USE_SPI_GYRO
 #endif
